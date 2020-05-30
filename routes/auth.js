e86f37ab5c9c274b2cb04d61b1c99dd117c222f5
Bishko <yurik322412@gmail.com>
2020-05-30 20:58:21 +0300

    creation of all routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const controller = require('../controllers/auth');
 const router = express.Router();
 
-router.get('/login', controller.login);
-router.get('/register', controller.register);
+router.post('/login', controller.login);
+router.post('/register', controller.register);
 
 module.exports = router;
