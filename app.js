839b43f64f8b437eb7f85e12aa00749fe4f2420f
Bishko <yurik322412@gmail.com>
2020-05-30 21:12:27 +0300

    parsing user data

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,9 @@
 module.exports.login = function (req, res) {
     res.status(200).json({
-        login: true
+        login: {
+            email: req.body.email,
+            password: req.body.password
+        }
     })
 }
 
