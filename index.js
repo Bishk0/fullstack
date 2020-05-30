8c55ee31acbb6246f1c8d51a41710c27efc28462
Bishko <yurik322412@gmail.com>
2020-05-30 19:46:47 +0300

    application launch setup

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-const express = require('express');
+const app = require('./app');
+const port = process.env.PORT || 5000;
 
-const app = express();
-
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Working'
-    })
-})
-
-app.listen(5000, () => console.log('Server has been started'))
+app.listen(port, () => console.log(`Server has been started on ${port}`));
