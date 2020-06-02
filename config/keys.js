7cfe2612411dc7ae867c0f89df099c7a6f7e9e83
Bishko <yurik322412@gmail.com>
2020-06-02 14:36:10 +0300

    login to the system

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://Bishko:Wew7e1njDMMYOPM8@cluster0-d6xbj.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://Bishko:Wew7e1njDMMYOPM8@cluster0-d6xbj.mongodb.net/test?retryWrites=true&w=majority',
+    jwt: 'dev-jwt'
 }
 
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,30 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const keys = require('../config/keys');
 
-module.exports.login = function (req, res) {
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password
+module.exports.login = async function (req, res) {
+   const candidate = await User.findOne({email: req.body.email});
+   if (candidate) {
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password);
+        if (passwordResult) {
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {expiresIn: 60 * 60});
+            res.status(200).json({
+                token: `Bearer ${token}`
+            })
+        } else {
+            res.status(401).json({
+                message: 'Паролі не співпадають. Спробуйте ще раз.'
+            })
         }
-    })
+   } else {
+       res.status(404).json({
+           message: 'Користувача з таким email не знайдено.'
+       });
+   }
 }
 
 module.exports.register = async function (req, res) {
