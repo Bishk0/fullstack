67d7eb8f9f63b54dc3a9948e09e64cd3cc4b2fa8
Bishko <yurik322412@gmail.com>
2020-06-02 17:22:41 +0300

    Universal error handling

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const keys = require('../config/keys');
+const errorHandler = require('../utils/errorHandler');
 
 module.exports.login = async function (req, res) {
    const candidate = await User.findOne({email: req.body.email});
@@ -45,7 +46,7 @@ module.exports.register = async function (req, res) {
             await user.save();
             res.status(201).json(user);
         } catch (e) {
-
+            errorHandler(res, e);
         }
     }
 }
