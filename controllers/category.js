79a6aa3c5974a76a8a0f8e8867a871d68e89091b
Bishko <yurik322412@gmail.com>
2020-06-02 23:24:44 +0300

    Retrieving and Deleting Categories

--- controllers/category.js
+++ controllers/category.js
@@ -1,19 +1,49 @@
-module.exports.getAll = function (req, res) {
-    res.json({message: 'category'})
+const Category = require('../models/Category');
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler');
+
+module.exports.getAll = async function (req, res) {
+    try {
+        const categories = await Category.find({user: req.user.id});
+        res.status(200).json(categories);
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
-module.exports.getById = function (req, res) {
-
+module.exports.getById = async function (req, res) {
+    try {
+        const category = await Category.findById(req.params.id);
+        res.status(200).json(category);
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
-module.exports.remove = function (req, res) {
-
+module.exports.remove = async function (req, res) {
+    try {
+        await Category.remove({_id: req.params.id});
+        await Position.remove({category: req.params.id});
+        res.status(200).json({
+            message: 'Категорія видалена.'
+        })
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
 module.exports.create = function (req, res) {
+    try {
 
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
 module.exports.update = function (req, res) {
+    try {
 
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
