9d54c6ece342cf388e5ba42d7cc3d06d1b52ef64
Bishko <yurik322412@gmail.com>
2020-06-02 21:07:19 +0300

    Getting all positions

--- controllers/position.js
+++ controllers/position.js
@@ -1,15 +1,38 @@
-module.exports.getByCategoryId = function (req, res) {
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler');
 
+module.exports.getByCategoryId = async function (req, res) {
+    try {
+        const  positions = await Position.find({
+            category: req.params.categoryId,
+            user: req.user.id
+        });
+        res.status(200).json(positions);
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
 module.exports.create = function (req, res) {
+    try {
 
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
 module.exports.remove = function (req, res) {
+    try {
 
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
 module.exports.update = function (req, res) {
+    try {
 
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
