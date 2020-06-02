9a86b8ea433c39fda30ac4f73e0d994df54d902e
Bishko <yurik322412@gmail.com>
2020-06-02 11:20:48 +0300

    User Creation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 module.exports.login = function (req, res) {
@@ -17,6 +18,17 @@ module.exports.register = async function (req, res) {
             message: 'Такий Email вже зайнятий. Спробуйте інший.',
         });
     } else {
+        const salt = bcrypt.genSaltSync(10);
+        const password = req.body.password;
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt),
+        })
+        try {
+            await user.save();
+            res.status(201).json(user);
+        } catch (e) {
 
+        }
     }
 }
