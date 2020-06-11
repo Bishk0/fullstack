1578ea3b4e7d4e2efd62b220da5caff931eb180c
Bishko <yurik322412@gmail.com>
2020-06-11 22:40:23 +0300

    Implement flow element output

--- client/src/app/categories-page/categories-page.component.ts
+++ client/src/app/categories-page/categories-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CategoriesService} from "../shared/services/categories.service";
 import {Category} from "../shared/interfaces";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-categories-page',
@@ -9,17 +10,12 @@ import {Category} from "../shared/interfaces";
 })
 export class CategoriesPageComponent implements OnInit {
 
-  loading = false;
-  categories: Category[] = [];
+  categories$: Observable<Category[]>;
 
   constructor(private categoriesService: CategoriesService) { }
 
   ngOnInit(): void {
-    this.loading = true;
-    this.categoriesService.fetch().subscribe(categories => {
-      this.loading = false;
-      this.categories = categories;
-    });
+    this.categories$ = this.categoriesService.fetch();
   }
 
 }
