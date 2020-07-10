32aa5b5c08fe2a13775cde8f3e15b532f4302e85
Bishko <yurik322412@gmail.com>
2020-07-11 00:08:30 +0300

    Preparing analytics page

--- client/src/app/analytics-page/analytics-page.component.ts
+++ client/src/app/analytics-page/analytics-page.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {AnalyticsService} from "../shared/services/analytics.service";
+import {AnalyticsPage} from "../shared/interfaces";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-analytics-page',
   templateUrl: './analytics-page.component.html',
   styleUrls: ['./analytics-page.component.css']
 })
-export class AnalyticsPageComponent implements OnInit {
+export class AnalyticsPageComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  constructor() { }
+  @ViewChild('gain') gainRef: ElementRef;
+  @ViewChild('order') orderRef: ElementRef;
+
+  average: number;
+  pending = true;
+  aSub: Subscription;
+
+  constructor(private analyticsService: AnalyticsService) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.aSub = this.analyticsService.getAnalytics().subscribe((data: AnalyticsPage) => {
+      this.average = data.average;
+
+      this.pending = false;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.aSub) {
+      this.aSub.unsubscribe();
+    }
   }
 
 }
--- client/src/app/shared/services/analytics.service.ts
+++ client/src/app/shared/services/analytics.service.ts
@@ -1,7 +1,7 @@
 import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
 import {Observable} from "rxjs";
-import {OverviewPage} from "../interfaces";
+import {OverviewPage, AnalyticsPage} from "../interfaces";
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +14,7 @@ export class AnalyticsService {
     return this.http.get<OverviewPage>('/api/analytics/overview');
   }
 
-  getAnalytics() {
-
+  getAnalytics(): Observable<AnalyticsPage> {
+    return this.http.get<AnalyticsPage>('/api/analytics/analytics');
   }
 }
